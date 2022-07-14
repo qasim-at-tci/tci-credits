436a4b752037bdb6ed182f3c1129b396ce480da5
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2022-07-14 18:38:38 +0500

    cleanup/dx

--- request.rb
+++ request.rb
@@ -2,9 +2,13 @@ require 'httparty'
 require 'json'
 require 'dotenv/load'
 
+# Travis API endpoint to fetch subscription data.
 URL = "https://api.travis-ci.com/v2_subscriptions"
+
+# Travis CI API token
 token = ENV['TRAVIS_TOKEN']
 
+# API call headers
 headers = { 
   "Authorization"  => "token #{token}",
   "Content-Type" => "application/json",
@@ -13,27 +17,33 @@ headers = {
   "path" => "/v2_subscriptions"
 }
 
-response = HTTParty.get(
+# Make API call if username(s) for End User / Organzation are set
+if ENV['USER_LOGIN'] || ENV['ORG_LOGIN']
+  response = HTTParty.get(
   URL,
   :query => {format: :json},
   :headers => headers
-)
+  )
 
-response['v2_subscriptions'].each do |sub|
-  if ENV['USER_LOGIN'] && sub['owner']['@type'] == "user" && sub['owner']['login'] == ENV['USER_LOGIN']
-    owner = sub['owner']['login']
-    remaining_credits = sub['addons'][0]['current_usage']['remaining']
+  # Iterate over found subscriptions and find corresponding user login and remaining credits
+  response['v2_subscriptions'].each do |sub|
+    if ENV['USER_LOGIN'] && sub['owner']['@type'] == "user" && sub['owner']['login'] == ENV['USER_LOGIN']
+      owner = sub['owner']['login']
+      remaining_credits = sub['addons'][0]['current_usage']['remaining']
 
-    p "***** USER ***************"
-    p "User: #{owner}"
-    p "Remaining Credits: #{remaining_credits}"
-    p "***************************"
-  elsif ENV['ORG_LOGIN'] && sub['owner']['@type'] == "organization" && sub['owner']['login'] == ENV['ORG_LOGIN']
-    owner = sub['owner']['login']
-    remaining_credits = sub['addons'][0]['current_usage']['remaining']
+      p "***** USER ***************"
+      p "User: #{owner}"
+      p "Remaining Credits: #{remaining_credits}"
+      p "***************************"
+    elsif ENV['ORG_LOGIN'] && sub['owner']['@type'] == "organization" && sub['owner']['login'] == ENV['ORG_LOGIN']
+      owner = sub['owner']['login']
+      remaining_credits = sub['addons'][0]['current_usage']['remaining']
 
-    p "***** Organization ***************"
-    p "Org: #{owner}"
-    p "Remaining Credits: #{remaining_credits}"
+      p "***** Organization ***************"
+      p "Org: #{owner}"
+      p "Remaining Credits: #{remaining_credits}"
+    end
   end
+else
+  p "No End user or Org username set in .env file!"
 end
